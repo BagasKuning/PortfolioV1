c3f84cd01264d81cdfe204fe7da36ce77e91b27d
Dwi Bagaskara <dwibagaskara55@gmail.com>
2023-01-31 23:07:41 +0700

    responsive

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const nav = document.getElementsByTagName('nav')[0]
 const desc = this.document.getElementById('desc')
 
+const descBefore = this.document.getElementsByClassName('desc-before')[0]
+const descAfter = this.document.getElementsByClassName('desc-after')[0]
+
+
 const descHeight = desc.offsetHeight / 4;
+
+desc.addEventListener('mouseenter', function(){
+    descBefore.style.animation = 'descBeforeAnim 1.1s ease-out forwards';
+    descAfter.style.animation = 'descAfterAnim 1.1s ease-out forwards';
+})
 
 window.addEventListener('scroll', function(){
     let x = window.scrollY
@@ -11,10 +20,21 @@ window.addEventListener('scroll', function(){
         nav.style.top = "0px";
         nav.style.animation = "navAnim .6s";
         nav.style.borderBottom = '3px solid whitesmoke';
-        // desc.style.marginTop = descHeight + 'px';
-    } else {
+    } else {  
         nav.style.position = "static";
         nav.removeAttribute('style');
         // desc.removeAttribute('style');
     }
+    
+    if(x < desc.offsetHeight - nav.offsetHeight){
+        descBefore.style.animation = 'descBeforeAnim 1.1s ease-out forwards';
+        descAfter.style.animation = 'descAfterAnim 1.1s ease-out forwards';
+    } else {
+        descBefore.removeAttribute('style')
+        descAfter.removeAttribute('style')
+    }
+
+
     })
+
+    

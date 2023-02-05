664f5b8186a9fe03dc966853e5b2781aef30b735
Dwi Bagaskara <dwibagaskara55@gmail.com>
2023-02-05 17:16:59 +0700

    nav animation

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const nav = document.getElementsByTagName('nav')[0]
 const desc = this.document.getElementById('desc')
+const nav2 = document.getElementsByClassName('nav')[0]
 
 const descBefore = this.document.getElementsByClassName('desc-before')[0]
 const descAfter = this.document.getElementsByClassName('desc-after')[0]
 
-
 const descHeight = desc.offsetHeight / 4;
+
+nav2.style.height = nav.offsetHeight + 'px';
+nav2.style.width = nav.offsetWidth + 'px';
+nav2.style.position = 'fixed';
 
 desc.addEventListener('mouseenter', function(){
     descBefore.style.animation = 'descBeforeAnim 1.1s ease-out forwards';
@@ -20,9 +24,11 @@ window.addEventListener('scroll', function(){
         nav.style.top = "0px";
         nav.style.animation = "navAnim .6s";
         nav.style.borderBottom = '3px solid whitesmoke';
+        nav2.style.position = 'relative';
     } else {  
         nav.style.position = "static";
         nav.removeAttribute('style');
+        nav2.style.position = 'fixed';
         // desc.removeAttribute('style');
     }
     
